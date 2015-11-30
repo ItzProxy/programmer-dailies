be080a0cfade04ac1069896923ca61b9962b3469
ItzProxy <allure.success@gmail.com>
2015-11-30 12:36:35 -0600

    Update ID3_algorithm.cpp

--- ID3_algorithm.cpp
+++ ID3_algorithm.cpp
@@ -4,26 +4,50 @@
 #include <math.h>
 
 using namespace std;
-float entropy(int a, b){
-   float entropy = (-1)*((a/b)log2(a/b));
+
+float entropy(int a, int b){
+   float l = log2((float)a/(float)b);
+   printf(":%f\n",l);
+   float entropy = -1*((float)a/(float)b)*l;
+   return entropy;
 }
-int main(){
+void entropyFinderMode(){
   int a,b,d;
   float* c;
   float total = 0;
-  printf("how many...");
+  printf("how many...\n");
   cin >> d;
   c = new float[d];
   for(int i = 0;i < d; i++){
-    printf("%d:enter a and b",i);
+    printf("%d:enter a and b\n",i);
     cin >> a >> b;
-    c[d] = entropy(a,b);
-    total += c[d];
+    c[i] = entropy(a,b);
+    total += c[i];
   }
  for(int i = 0; i < d; i++){
-   printf("%d = %f\n", c[i]);
+   printf("%d = %f\n", i, c[i]);
  }
   printf("total = %f\n", total);
-  
+}
+int main(){
+  int mode;
+  do{
+    printf("Select mode:");
+    cin >> mode;
+    switch(mode){
+      case 1:
+          entropyFinderMode();
+          break;
+      case 2:
+          printf("hello\n");
+          break;
+      case 0:
+          printf("Exiting...\n");
+          exit(0);
+      default:
+        printf("invalid command\n");
+    }
+    
+  }while(mode != 0);
   return 0;
 }
