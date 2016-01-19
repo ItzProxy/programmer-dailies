0db80aedb9adf4b801ef3602be10a6155f99db86
ItzProxy <allure.success@gmail.com>
2016-01-19 12:03:02 -0600

    Create challenge248.cs

---
+++ challenge248.cs
@@ -0,0 +1,33 @@
+using System;
+
+public struct building
+{
+	public string building_name;
+	public float initial_cost;
+	public int cookies_generate_base;
+	public int cookies_generate_offset;
+	public float upgrade_rate;
+}
+
+abstract class building_methods
+{
+	string get_name
+}
+
+class constructor : building_methods
+{
+}
+
+public class clicker_simulator
+{
+	private int building_num;
+	private building[] builds = new building[8];
+}
+
+public class Program
+{
+	public static void Main()
+	{
+		building a;
+	}
+}
