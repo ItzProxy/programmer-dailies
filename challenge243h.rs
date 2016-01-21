566fd13d92ac4e79390b09ff81bb63f59fa0bbf1
ItzProxy <allure.success@gmail.com>
2016-01-21 12:09:31 -0600

    Update challenge243h.rs

--- challenge243h.rs
+++ challenge243h.rs
@@ -1,5 +1,16 @@
 //lets try this using rust
 //[2015-12-04] Challenge #243 [Hard] New York Street Sweeper Paths
+//https://github.com/nrc/r4cppp/blob/master/graphs/README.md
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::HashSet;
+
+use io::string;
+
+struct Node {
+    datum: &'static str,
+    edges: Vec<Rc<RefCell<Node>>>,
+} //node structure
 
 fn main(){
   
